917fdea504a66c1e501d9b65230b014775272fed
Basharat Ali <basharatalidogar14@gmail.com>
2024-01-15 13:57:35 +0500

    edit readme and add new.md

---
+++ assets/js/tailwind.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        container: {
+            padding: {
+              DEFAULT: '1rem',
+              sm: '2rem',
+              lg: '4rem',
+              xl: '5rem',
+              '2xl': '6rem',
+            },
+            center:true,
+          },
+        colors: {
+          clifford: '#da373d',
+        }
+      }
+    }
+  }
